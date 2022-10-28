9c04eef92fc24c55c11afdeefb709d27357adf2f
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-28 21:11:22 +0300

    Домашняя работа к 4 семинару, задача 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Задание должно быть выполнено в методах.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]; 6, 1, 33 -> [6, 1, 33]*/
+
+Console.Clear();
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите один элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("Вывод массива: ");
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+
+FillArray(array);
