79e4086c9b7beda1005ffd2474a688221b8ce924
Nikita Labinskiy <NLabinskiy@gmail.com>
2022-11-12 23:23:56 +0300

    Семинар 5 зазадача 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] arr = new int[6];
+
+FillArray(arr);
+
+PrintArray(arr);
+
+Console.WriteLine();
+
+int result = EvenNumbers(arr);
+
+Console.Write(result);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+}
+
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+          count++;  
+        } 
+    }
+    return count;
+}
